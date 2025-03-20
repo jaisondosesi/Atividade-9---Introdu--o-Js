0eff7c80e59deb6d6086edbcaa6a5c0b0934f9a2
jaisondosesi <jaison_conaco-jun@estudante.sesisenai.org.br>
2025-03-20 09:53:02 -0300

    add

---
+++ script.js
@@ -0,0 +1,36 @@
+
+/*
+Atv - 1
+console.log('hello world');
+let numero = prompt('Digite seu número');
+function verificarPar(numero){
+        if(numero % 2 === 0){
+            console.log(numero + " é igual a par.");
+        }else{ 
+            console.log(numero + " é igual a ímpar");
+        }
+}
+verificarPar(numero);
+*/
+
+/*
+let n1 = parseFloat(prompt('Escreva n1:'))
+let n2 = parseFloat(prompt('Escreva n2:'))
+
+function calcular(num1, num2) {
+    const soma = num1 + num2;
+    const subtracao = num1 - num2;
+    const multiplicacao = num1 * num2;
+    console.log(soma)
+    console.log(subtracao)
+    console.log(multiplicacao)
+    if (num2 === 0) {
+        console.log("Não da pra dividir por 0!")
+    }else {
+        const divisao = num1 / num2;
+        console.log(divisao)
+    }
+    
+    }
+calcular(n1, n2);
+*/
