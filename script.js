c03396af49a25cfe8ae39661bda35b2c8cc18f58
jaisondosesi <jaison_conaco-jun@estudante.sesisenai.org.br>
2025-03-23 22:07:03 -0300

    add

--- script.js
+++ script.js
@@ -112,7 +112,30 @@ function media() {
 media()
 */
 
+/*
+Questão 9
+let mult3 = [];
+let nmult3 = [];
+let a = 0;
+let b = 0;
 
+for (let i = 1; i < 21; i++) {
+    if(i % 3 == 0){
+        mult3[a] = i;
+        a++;
+    } else {
+        nmult3[b] = i;
+        b++;
+    }
+} 
+
+function tabelation() {
+    console.log("Multiplos de 3: " + mult3);
+    console.log("Não multiplos de 3: " + nmult3);
+}
+
+tabelation()
+*/
 
 
 
